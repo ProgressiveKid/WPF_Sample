99fa2da4424170b84c6ea46786f1950455cce6df
Влад Голуб <konus228@mail.ru>
2022-08-29 13:36:52 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           DataGridView.RowBackground
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+
+            MessageBox.Show("Jope");
+            TextBox1.Text = "Button has been pressed";
+            
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class User
+    {
+        public int Rank { get; set; }
+        public string UserName { get; set; }
+        public string Status { get; set; }
+        public int Steps { get; set; }
+    
+
+    }
+}
