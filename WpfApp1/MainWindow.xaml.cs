11275dd88dcc9a69dbe99814446a4efdd9ac2e91
Влад Голуб <konus228@mail.ru>
2022-09-19 20:53:07 +0300

    I made a diagram output

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using LiveCharts;
 using LiveCharts.Wpf;
 using System.Reflection.Emit;
 using LiveCharts.Definitions.Charts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf.Charts.Base;
 
 namespace WpfApp1
 {
@@ -25,16 +27,20 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-       public SeriesCollection SeriesCollection { get; }
+       public SeriesCollection SeriesCollection { get; set;}
+        public ChartValues<double> Values1 { get; set; }
+        public ChartValues<double> Values2 { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-           
-        }
-    
 
-      private void Button_Click(object sender, RoutedEventArgs e)
+
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             ReaderFromJson readerFromJson = new ReaderFromJson();
             List<UserInTable> listPeople = readerFromJson.ReadFromJsonFile();          
@@ -59,108 +65,73 @@ namespace WpfApp1
 
         public int [] DataForChart()
         {
-            UserInTable userInTable = (UserInTable)DataGridView.SelectedItem;
-            string nameFromGridView = userInTable.UserName;
-
-            ReaderFromJson readerFromJson = new ReaderFromJson();
-            var BigUser = readerFromJson.ReadFromJsonFile();
-            var ListForGrid = ReaderFromJson.ListForGridView; // Копия BigUser - лист со всеми значениями ищ папки JSON
-            int[] DayWithValue = new int[userInTable.CountDay];
-            int countDay = 0;
-            foreach (var Data in ListForGrid)
-            {
-                if (Data.UserName == nameFromGridView)
+           
+                UserInTable userInTable = (UserInTable)DataGridView.SelectedItem;
+                string nameA = userInTable.UserName;
+                string nameFromGridView = nameA;
+                ReaderFromJson readerFromJson = new ReaderFromJson();
+                var BigUser = readerFromJson.ReadFromJsonFile();
+                var ListForGrid = ReaderFromJson.ListForGridView; // Копия BigUser - лист со всеми значениями ищ папки JSON
+                int[] DayWithValue = new int[userInTable.CountDay];
+                int countDay = 0;
+                foreach (var Data in ListForGrid)
                 {
-                    DayWithValue[countDay] = Data.Steps;
-                    countDay++;
+                    if (Data.UserName == nameFromGridView)
+                    {
+                        DayWithValue[countDay] = Data.Steps;
+                        countDay++;
+                    }
                 }
-            }
-            return DayWithValue;
+               
+                return DayWithValue;
+          
         }
 
         private void DataGridView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            // MessageBox.Show("");
-
-
             int[] ints = DataForChart();
-            int n = ints[4];
-            MessageBox.Show(Convert.ToString(n));
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e) // логика построения графика
-        {
             ReaderFromJson readerFromJson = new ReaderFromJson();
-            // readerFromJson.ShowChart();
-            List <UserInTable> ChartList = readerFromJson.ReadFromJsonFile();
-            //Chart.Series.Clear();
+
+            List<UserInTable> ChartList = readerFromJson.ReadFromJsonFile();
             SeriesCollection series = new SeriesCollection();
-            var years = (from o in ChartList as List<UserInTable> select new { years = o.CountDay }).Distinct();
-            foreach (var year in years)
+            ChartValues<int> ass = new ChartValues<int>();
+            for (int i = 0; i < ints.Count(); i++)
             {
-                List<double> values = new List<double>();
-                for (int i = 0; i < 10; i++)
-                {
-                    int iss = 5;
-                    values.Add(iss+i*0.3); 
-                
-                }
-                    series.Add(new LineSeries() { Title = year.years.ToString(), Values = new ChartValues<double>(values) });
+                ass.Add(ints[i]);
+                series.Add(new LineSeries() { Title = i.ToString(), Values = ass });
             }
             Chart.Series = series;
+        }
+        ChartValues<ObservablePoint> series = new ChartValues<ObservablePoint>();
+        private void Button_Click_1(object sender, RoutedEventArgs e) // логика построения графика
+        {
+            
+                int[] ints = DataForChart();
+                ReaderFromJson readerFromJson = new ReaderFromJson();
+
+                List<UserInTable> ChartList = readerFromJson.ReadFromJsonFile();
+
+                SeriesCollection series = new SeriesCollection();
+            ChartValues<int> ass = new ChartValues<int>();
+                for (int i = 0; i < ints.Count(); i++)
+                {
+
+                   
+                ass.Add(ints[i]);
+                    series.Add(new LineSeries() { Title = i.ToString(), Values = ass });
+                }
+            Chart.Series = series;
+           
         }
         // нужен метод который считыват строку выбирает от туда имя пользователя ищет его в бигюзере потом идёт в метод на верху
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            /*
-            Chart.Series.Add(new VerticalLineSeries
-            {
-                Values = new ChartValues<double> { 3, 5, 2, 6, 2, 7, 1 }
-            });
-
-            Chart.Series.Add(new RowSeries
-            {
-                Values = new ChartValues<double> { 6, 2, 6, 3, 2, 7, 2 }
-            });
-
-            Chart.AxisY.Add(new Axis
-            {
-               // Separator = new Separator { Step = 1 }
-            });
-
-            Chart.AxisX.Add(new Axis
-            {
-                MinValue = 0
-            });
-
-            var tooltip = new DefaultTooltip
-            {
-                SelectionMode = TooltipSelectionMode.SharedYValues
-            };
-
-            Chart.DataTooltip = tooltip;
-            // int[] ints = DataForChart();
-            //int CountDay = ints.Count();
-            /*
-            Chart.AxisX.Add(new LiveCharts.Wpf.Axis
-            {
-                Title = "День",
-                
-                Labels = new[] {
-                 
-                    "Jan","Feb", "Хочяеь 3 месяц", "4 сццц"}
-            }
-            );
-            Chart.AxisY.Add(new LiveCharts.Wpf.Axis
-            {
-                Title = "Значения",
-                LabelFormatter = value => value.ToString("C")
-            }
-            );
-            */
-            Chart.LegendLocation = LiveCharts.LegendLocation.Right;
+          
         }
 
-        
+        private void DataGridView_AutoGeneratedColumns(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
--- WpfApp1/UserInTable.cs
+++ WpfApp1/UserInTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,8 @@ namespace WpfApp1
 {
     internal class UserInTable
     {
-       // public int Rank { get; set; }
-        
+        // public int Rank { get; set; }
+        [DisplayName("Имя пользователя")]
         public string UserName { get; set; }
         public int CountDay { get; set; }
         public int SumStepsDay { get; set; }
